71d326897ba2ea734435fad78702301f4ea87d1c
Patrick <patrick@LAPTOP-RDQGM0DJ.localdomain>
2024-06-03 12:44:02 +0200

    Added function for averaging sensor value

--- Capacity/capaSensorTranspiratie/capaSensorTranspiratie.ino
+++ Capacity/capaSensorTranspiratie/capaSensorTranspiratie.ino
@@ -10,23 +10,31 @@
 
 #define sensorPin A0
 
-int sensorValue = 0;  // int to save sensorvalue
-int calibrateValue = 473; // int to calibrate dry state 
-int wetValue = 212; // int to calibrate wet state
-int humidity = 0; // int to save humidity calculations
 int calibratedSkinCapacity = 0; // variable to store calibrated skin capacity without stress
 
-
+// get a average reading from sensordata over x amount op measurments
 int GetAverageSensorValue(int measurmentCount)
 {
-  //int measurmentCount = 100;
+  int sensorValue = 0;  // int to save sensorvalue
+  int sumSensorValue = 0; // reset sum everytime function gets called
+  
   for(int i=0;i<measurmentCount;i++) // peforme measurment a copple of time to get average
   {
     sensorValue = analogRead(sensorPin);  //Read the sensor value
-    int sumSensorValue += sensorValue; //sum all measured sensorValues
+    sumSensorValue += sensorValue; //sum all measured sensorValues
   }
   averageSensorValue = sumSensorValue / measurmentCount; // Dived the sum of sensorValues by measurements to get average
   return averageSensorValue;
+}
+
+
+void CalibrateSkinCapacity()
+{
+  Serial.println("Calibrating skin capacity...");
+  calibratedSkinCapacity = GetAverageSensorValue(100); //Perform a skin capacity calibration by getting average skin capacity over 100 measurments
+  Serial.print("Skin capacity calibrated at: ");
+  Serial.println(calibratedSkinCapacity);
+  delay(500);
 }
 
 
@@ -34,20 +42,14 @@ void setup()
 { 
   Serial.begin(9600);
   delay(500);
-  Serial.println("Reading sensorvalues");
-  calibratedSkinCapacity = GetAverageSensorValue(100); //Perform a skin capacity calibration by getting average skin capacity over 100 measurments
+  CalibrateSkinCapacity();
 }
 
 
 void loop()
 {
-  averagesensorValue = analogRead(sensorPin);  //Read the sensor value
-  humidity = map(sensorValue, wetValue, dryValue, 100, 0);  //map de sensor value as a percentage of full wet and full dry
-  Serial.print("SensorValue= ");
-  Serial.print(sensorValue);  //print sensor value
-  Serial.print(" | ");  //Space out the values
-  Serial.print("Humidity= ");
-  Serial.print(humidity); //print humidity
-  Serial.println("%");
+  int currentSkinCapacity = GetAverageSensorValue(50);
+  Serial.print("Skin Capacity= ");
+  Serial.print(currentSkinCapacity);  //print sensor value
   delay(500); //Wait half a second before next reading
 }
