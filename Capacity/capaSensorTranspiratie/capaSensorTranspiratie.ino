967ffa334977e4ddba9b9518964a15202e0be079
Patrick Schuurman <patrick@deschuurtjes.com>
2024-06-04 13:31:42 +0200

    Update capaSensorTranspiratie.ino

--- Capacity/capaSensorTranspiratie/capaSensorTranspiratie.ino
+++ Capacity/capaSensorTranspiratie/capaSensorTranspiratie.ino
@@ -10,26 +10,27 @@
 
 #define sensorPin A0
 
-int calibratedSkinCapacity = 0; // variable to store calibrated skin capacity without stress
+long calibratedSkinCapacity = 0; // variable to store calibrated skin capacity without stress
 
 // get a average reading from sensordata over x amount op measurments
 int GetAverageSensorValue(int measurmentCount)
 {
   int sensorValue = 0;  // int to save sensorvalue
-  int sumSensorValue = 0; // reset sum everytime function gets called
+  long sumSensorValue = 0; // reset sum everytime function gets called
   
   for(int i=0;i<measurmentCount;i++) // peforme measurment a copple of time to get average
   {
     sensorValue = analogRead(sensorPin);  //Read the sensor value
     sumSensorValue += sensorValue; //sum all measured sensorValues
   }
-  averageSensorValue = sumSensorValue / measurmentCount; // Dived the sum of sensorValues by measurements to get average
+  long averageSensorValue = sumSensorValue / measurmentCount; // Dived the sum of sensorValues by measurements to get average
   return averageSensorValue;
 }
 
 
 void CalibrateSkinCapacity()
 {
+  delay(1000);
   Serial.println("Calibrating skin capacity...");
   calibratedSkinCapacity = GetAverageSensorValue(100); //Perform a skin capacity calibration by getting average skin capacity over 100 measurments
   Serial.print("Skin capacity calibrated at: ");
@@ -48,8 +49,27 @@ void setup()
 
 void loop()
 {
-  int currentSkinCapacity = GetAverageSensorValue(50);
-  Serial.print("Skin Capacity= ");
-  Serial.print(currentSkinCapacity);  //print sensor value
+  int currentSkinCapacity = GetAverageSensorValue(50);  // get de a average of the current sensorvalue
+  int difference = calibratedSkinCapacity - currentSkinCapacity;  // calculate the difference between clabratedValue and current to detect transpiration
+  int diffPerc = map(difference,0,calibratedSkinCapacity,0,100); // Transfere diff into percantage
+
+  // when sensor doesnt make good contact with skin give ERROR otherwise continue
+  if(difference < 0)
+  {
+    Serial.println("Sensor ERROR, Please be sure sensor makes contact"); // sensor doesnt make good contact
+  }
+  else
+  {
+    Serial.print("Skin Capacity= ");
+    Serial.print(currentSkinCapacity);  //print sensor value
+    Serial.print(" | ");
+    Serial.print("Diff: ");
+    Serial.print(difference); // print differnece between calc en current value
+    Serial.print(" | ");
+    Serial.print("diffPerc: ");
+    Serial.print(diffPerc);
+    Serial.println("%");
+  }
+
   delay(500); //Wait half a second before next reading
 }
