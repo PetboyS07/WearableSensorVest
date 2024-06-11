fffb1d20df75b912710e28a1abec682563f19f68
JoonT0M26 <21005133@student.hhs.nl>
2024-06-11 12:09:49 +0200

    Update GSR_sensor_V2.ino
    y = aX, wrong reading toegevoegd

--- GSR/GSR_Sensor_V2/GSR_sensor_V2/GSR_sensor_V2.ino
+++ GSR/GSR_Sensor_V2/GSR_sensor_V2/GSR_sensor_V2.ino
@@ -1,16 +1,23 @@
 //GSR Grove sensor
-//Sensor zit op de huid voor de kalabratie, voordat het systeem wordt gestart.
+//Sensor sits on the skin for calabration, before starting the system.
+
 const int GSR = A1;
+const int measurments = 100;
 
 int sensorValue = 0;
-float gsr_average = 0;
+int gsr_average = 0;
 int SerialCalibration = 0;
 int HumanResistance = 0;
-const int measurments = 100;
-int difference;
-int sumcal = 0;
 
-int Sumcal(){      //Average the x measurements to remove the glitch
+double scale = 0;
+double sumcal = 0;
+
+//int difference;
+//float current_ADC = 0.0015;             //current through ADC in Ampere
+//float voltage_ADC;                      // output sensor calculate back to voltage in volts //double?
+//int offset = 0;                         //offset
+
+int Sumcal(){                           //Average the x measurements to remove the glitch
   int sumcal = 0;
   for(int i = 0; i < measurments; i++)
   {
@@ -18,14 +25,14 @@ int Sumcal(){      //Average the x measurements to remove the glitch
   sumcal += sensorValue;
   delay(5);
   }
-  return abs(sumcal / measurments);
+  return (sumcal / measurments);
 }
 
 void setup()
 {
   Serial.begin(9600);
   
-  SerialCalibration = Sumcal();             //bepaal waarde van de droge huid
+  SerialCalibration = Sumcal();         //determine once the mean value of dry skin
   //Serial.print("SerialCalibration= ");
   //Serial.println(SerialCalibration);
 }
@@ -33,20 +40,33 @@ void setup()
 void loop()
 {
   gsr_average = Sumcal();
-  difference = abs(SerialCalibration - gsr_average);    //groot verschil;, veel zweet
-  HumanResistance = map(difference, 0, SerialCalibration, 0, 100); //0 < HR < 100 procent
+  scale = map(gsr_average, 0, SerialCalibration, 0, 1023);          
+  HumanResistance = abs((30000 * scale) / 1023);            //30 kOhm is max skin resistance
+  //difference = abs(SerialCalibration - gsr_average);      //huge difference, lots of sweat
 
+  //p 1023   pergentage
+  //r 30000  HR?
+  
+  if(HumanResistance > 0)
+  {
+    Serial.print("HR = ");
+    Serial.print(HumanResistance);
+    Serial.println(" Ohm");
+  }
+  else{Serial.println("Wrong Reading");}
+
+}
+
+//voltage_ADC = map(gsr_average, 0, 1023, 0, ); //
+  //HumanResistance = voltage_ADC / current_ADC + offset; //y = ax + b in Ohm. R = U/I + offset
+  
+  //HumanResistance = map(difference, 0, 1023, 0, 100);   //0 < HR < 100 procent
   //HumanResistance = (difference / SerialCalibration) * 100; //0 < HR < 100 procent
-  //HumanResistance = ((1024 + 2 * gsr_average) * 10000) / difference; //in ohm, maar zeer onnauwkeurig
+  //HumanResistance = ((1024 + 2 * gsr_average) / difference) * 10000;    //Pure nonsense!!! in ohms, but very inaccurate/unreliable
+  
   
   //Serial.print("Reading= ");
-  //Serial.println(sensorValue);
-  //sensorValue = analogRead(GSR);
+  //Serial.println(gsr_average);
 
   //Serial.print("Difference= ");
   //Serial.println(difference);
-
-  Serial.print("HR= ");
-  Serial.println(HumanResistance);
-
-}
